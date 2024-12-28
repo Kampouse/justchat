a5cd3bd60885cbe2a597e3e91cf875c29a1120c2
jean-philippe martel <jpmartel98@gmail.com>
2024-12-28 16:45:51 -0500

    ugh should work now

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -4,10 +4,8 @@ import { drizzle } from "drizzle-orm/libsql";
 
 export default () => {
   const sqlite = createClient({
-    url:
-      process.env.NODE_ENV === "production"
-        ? (process.env.DATABASE_URL as string)
-        : "http://localhost:8080",
+    url: process.env.DATABASE_URL as string,
+
     authToken: process.env.AUTH_TOKEN,
   });
   const db = drizzle(sqlite, { schema });
