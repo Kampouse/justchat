c71bcae741b39cfab0107fd690e1c8008fa8333f
jean-philippe martel <jpmartel98@gmail.com>
2024-12-29 15:02:17 -0500

    would be dumb

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -5,7 +5,10 @@ import { drizzle } from "drizzle-orm/libsql";
 export default () => {
   console.log(process.env.DATABASE_URL, process.env.AUTH_TOKEN);
   const sqlite = createClient({
-    url: process.env.DATABASE_URL as string,
+    url:
+      process.env.NODE_ENV === "production"
+        ? "libsql://just-chat-kampouse.turso.io"
+        : "http://localhost:8080",
 
     authToken: process.env.AUTH_TOKEN,
   });
