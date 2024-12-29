0092199b3f1521a1aae8f74cdbdf32bb78d1997e
jean-philippe martel <jpmartel98@gmail.com>
2024-12-29 10:44:44 -0500

    something wrong

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -3,6 +3,7 @@ import { schema } from "./schema";
 import { drizzle } from "drizzle-orm/libsql";
 
 export default () => {
+  console.log(process.env.DATABASE_URL, process.env.AUTH_TOKEN);
   const sqlite = createClient({
     url: process.env.DATABASE_URL as string,
 
