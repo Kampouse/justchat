34af69871be79ba8b0320233892871a37fa464c0
jean-philippe martel <jpmartel98@gmail.com>
2025-02-27 17:20:27 -0500

    added google auth

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,10 +1,14 @@
 import { QwikAuth$ } from "@auth/qwik";
 import GitHub from "@auth/qwik/providers/github";
+import Google from "@auth/qwik/providers/google";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
     providers: [
       GitHub({
+        checks: ["none"],
+      }),
+      Google({
         checks: ["none"],
       }),
     ],
