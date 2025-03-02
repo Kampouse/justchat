e9aadf9480a3d68bf1c3a14639a18460c1edcea8
jean-philippe martel <jpmartel98@gmail.com>
2025-03-02 13:18:49 -0500

    added file

---
+++ src/routes/chat/[id]/layout.tsx
@@ -0,0 +1,52 @@
+import { routeLoader$ } from "@builder.io/qwik-city";
+import type { Session } from "~/server";
+import {
+  getAllMessages,
+  getConvoByUuid,
+  getConvos,
+  GetRemainingQueries,
+  getUser,
+} from "~/server";
+import { type Message } from "~/routes/api";
+
+export const useMessages = routeLoader$(async (e) => {
+  const ctx = e.sharedMap.get("session") as Session | null;
+  const uuid = e.params.id;
+
+  const messages = await getAllMessages({ ctx, uuid });
+  if (!messages) throw e.redirect(302, "/");
+
+  return messages.map((el) => ({
+    type: el.type,
+    content: el.content,
+  })) as Message[];
+});
+
+export const useServerSessio = routeLoader$(async (e) => {
+  const session = e.sharedMap.get("session") as Session | null;
+
+  const user = await getUser(session);
+  return { user, session };
+});
+
+export const useConved = routeLoader$(async (e) => {
+  const session = e.sharedMap.get("session") as Session | null;
+  return (await getConvos(session)) ?? [];
+});
+
+export const useRemainingQueires = routeLoader$(async (e) => {
+  const session = e.sharedMap.get("session") as Session | null;
+
+  if (!session) return 0;
+  const data = await GetRemainingQueries(session);
+  if (data == null) {
+    return 0;
+  }
+  return data;
+});
+
+export const useTitle = routeLoader$(async (e) => {
+  const ctx = e.sharedMap.get("session") as Session;
+  const uuid = e.params["id"];
+  return await getConvoByUuid({ ctx, uuid });
+});
